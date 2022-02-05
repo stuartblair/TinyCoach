2fb83add4b963244e8e30d326b40c15ccdf83436
Stuart Blair <stuart.a.blair@gmail.com>
2022-02-05 14:58:18 -0800

    Updating the message for the main app.

--- main/TinyCoach.c
+++ main/TinyCoach.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <mpu6050.h>
 
+static void print_banner(const char* text);
+
 void app_main(void)
 {
-  printf("Hello, I'm your TinyCoach");
+    print_banner("Hello, I'm your TinyCoach");
+}
+
+static void print_banner(const char* text)
+{
+    printf("\n#### %s #####\n\n", text);
 }
